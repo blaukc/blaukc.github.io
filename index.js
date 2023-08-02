0e86827791e78e5d7f201aab5119243dfb34da9a
blaukc <brandonlau1@hotmail.com>
2023-08-02 18:41:06 +0800

    Add computer scroll animation

--- index.js
+++ index.js
@@ -1,53 +1,46 @@
-import * as THREE from 'three';
+const computer = document.getElementById("computer");
+const keyboard = document.getElementById("keyboard");
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scrollComputerUpBreakpoint = 500;
+const pauseComputerBreakpoint = 750;
+const splitComputerBreakpoint = 2000;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const computerScrollUpSpeed = 0.5;
+const splitComputerSpeed = 0.5;
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const computer = new THREE.Mesh( geometry, material );
-scene.add( computer );
+const scrollComputerUp = (scrollDistance) => {
+    const y = scrollDistance * computerScrollUpSpeed;
 
-computer.position.z = -10;
-
-const computerFinalPosition = {
-    rotationX: 0,
-    rotationY: -1.4,
-    rotationZ: 0,
-    positionX: 3,
-    positionY: 0,
-    positionZ: -5
-}
-const speed = 0.02
-
-// computer.position.z = computerFinalPosition.positionZ;
-// computer.position.x = computerFinalPosition.positionX;
-// computer.rotation.y = computerFinalPosition.rotationY;
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
-	// cube.rotation.z += 0.01;
-    if (computer.position.x < computerFinalPosition.positionX) {
-        const difference = Math.abs(computer.position.x - computerFinalPosition.positionX);
-        computer.position.x += difference * speed;
-    }
-    if (computer.position.z < computerFinalPosition.positionZ) {
-        const difference = Math.abs(computer.position.z - computerFinalPosition.positionZ);
-        computer.position.z += difference * speed;
-    }
-    if (computer.rotation.y > computerFinalPosition.rotationY) {
-        const difference = Math.abs(computer.rotation.y - computerFinalPosition.rotationY);
-        computer.rotation.y -= difference * speed;
-    }
-
-	renderer.render( scene, camera );
+    computer.style.transform = 'translate3d(0px, ' + y + 'px, 0px)';
+    keyboard.style.transform = 'translate3d(0px, ' + y + 'px, 0px)';
 }
 
-animate();
+const pauseComputer = (scrollDistance, x) => {
+    const y = scrollDistance - scrollComputerUpBreakpoint * computerScrollUpSpeed;
+
+    computer.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0px)';
+    keyboard.style.transform = 'translate3d(' + -x + 'px, ' + y + 'px, 0px)';
+}
+
+const splitComputer = (scrollDistance) => {
+    const x = (scrollDistance - pauseComputerBreakpoint) * splitComputerSpeed;
+    const y = scrollDistance - scrollComputerUpBreakpoint * computerScrollUpSpeed;
+
+    computer.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0px)';
+    keyboard.style.transform = 'translate3d(' + -x + 'px, ' + y + 'px, 0px)';
+}
+
+document.addEventListener("scroll", (evt) => {
+    const scrollDistance = window.scrollY;
+
+    if (scrollDistance < scrollComputerUpBreakpoint) {
+        scrollComputerUp(scrollDistance);
+    } else if (scrollDistance < pauseComputerBreakpoint) {
+        pauseComputer(scrollDistance, 0);
+    } else if (scrollDistance < splitComputerBreakpoint) {
+        splitComputer(scrollDistance);
+    } else {
+        const x = (splitComputerBreakpoint - pauseComputerBreakpoint) * splitComputerSpeed;
+        pauseComputer(scrollDistance, x);
+    }
+});
