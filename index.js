3005ff3da1936241c7497796a4f7041c3291304b
blaukc <brandonlau1@hotmail.com>
2023-08-02 20:38:26 +0800

    Use scale instead of height width

--- index.js
+++ index.js
@@ -2,46 +2,36 @@ const computer = document.getElementById("computer");
 const keyboard = document.getElementById("keyboard");
 
 const scrollComputerUpBreakpoint = 500;
-const pauseComputerBreakpoint = 1000;
-const splitComputerBreakpoint = 2250;
+const pauseComputerBreakpoint = 850;
+const splitComputerBreakpoint = 2150;
 
 const computerScrollUpSpeed = 0.5;
-const splitComputerSpeed = 0.5;
-const computerSizeUpSpeed = 0.005;
-
-const computerSize = 25;
-computer.style.height = computerSize + 'vh';
-computer.style.width = computerSize + 'vh';
-keyboard.style.height = computerSize + 'vh';
-keyboard.style.width = computerSize + 'vh';
+const splitComputerSpeed = 0.55;
+const computerSizeUpSpeed = 0.0003;
 
 const scrollComputerUp = (scrollDistance) => {
     const y = scrollDistance * computerScrollUpSpeed;
 
-    computer.style.transform = 'translate3d(0px, ' + y + 'px, 0px)';
-    keyboard.style.transform = 'translate3d(0px, ' + y + 'px, 0px)';
+    computer.style.transform = 'translate3d(0px, ' + y + 'px, 0px) scale(1)';
+    keyboard.style.transform = 'translate3d(0px, ' + y + 'px, 0px) scale(1)';
 }
 
-const pauseComputer = (scrollDistance, x) => {
+const pauseComputer = (scrollDistance, x, scale) => {
     const y = scrollDistance - scrollComputerUpBreakpoint * computerScrollUpSpeed;
 
-    computer.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0px)';
-    keyboard.style.transform = 'translate3d(' + -x + 'px, ' + y + 'px, 0px)';
+    computer.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0px) scale(' + scale + ')';
+    keyboard.style.transform = 'translate3d(' + -x + 'px, ' + y + 'px, 0px) scale(' + scale + ')';
 }
 
 const splitComputer = (scrollDistance) => {
     const scrollDifference = scrollDistance - pauseComputerBreakpoint
     const x = scrollDifference * splitComputerSpeed;
     const y = scrollDistance - scrollComputerUpBreakpoint * computerScrollUpSpeed;
+    const scale = 1 + scrollDifference * computerSizeUpSpeed;
 
-    computer.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0px)';
-    keyboard.style.transform = 'translate3d(' + -x + 'px, ' + y + 'px, 0px)';
+    computer.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0px) scale(' + scale + ')';
+    keyboard.style.transform = 'translate3d(' + -x + 'px, ' + y + 'px, 0px) scale(' + scale + ')';
 
-    const newComputerSize = computerSize + scrollDifference * computerSizeUpSpeed;
-    computer.style.height = newComputerSize + 'vh';
-    computer.style.width = newComputerSize + 'vh';
-    keyboard.style.height = newComputerSize + 'vh';
-    keyboard.style.width = newComputerSize + 'vh';
 }
 
 document.addEventListener("scroll", (evt) => {
@@ -50,11 +40,13 @@ document.addEventListener("scroll", (evt) => {
     if (scrollDistance < scrollComputerUpBreakpoint) {
         scrollComputerUp(scrollDistance);
     } else if (scrollDistance < pauseComputerBreakpoint) {
-        pauseComputer(scrollDistance, 0);
+        pauseComputer(scrollDistance, 0, 1);
     } else if (scrollDistance < splitComputerBreakpoint) {
         splitComputer(scrollDistance);
     } else {
-        const x = (splitComputerBreakpoint - pauseComputerBreakpoint) * splitComputerSpeed;
-        pauseComputer(scrollDistance, x);
+        const scrollDifference = splitComputerBreakpoint - pauseComputerBreakpoint
+        const x = (scrollDifference) * splitComputerSpeed;
+        const scale = 1 + (scrollDifference) * computerSizeUpSpeed;
+        pauseComputer(scrollDistance, x, scale);
     }
 });
