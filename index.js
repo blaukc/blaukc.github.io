92c2aa77f2b3558c667b88dfe9030d33835e403e
blaukc <brandonlau1@hotmail.com>
2023-08-02 19:08:54 +0800

    Increase size of computer

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const computer = document.getElementById("computer");
 const keyboard = document.getElementById("keyboard");
 
 const scrollComputerUpBreakpoint = 500;
-const pauseComputerBreakpoint = 750;
-const splitComputerBreakpoint = 2000;
+const pauseComputerBreakpoint = 1000;
+const splitComputerBreakpoint = 2250;
 
 const computerScrollUpSpeed = 0.5;
 const splitComputerSpeed = 0.5;
+const computerSizeUpSpeed = 0.005;
+
+const computerSize = 25;
+computer.style.height = computerSize + 'vh';
+computer.style.width = computerSize + 'vh';
+keyboard.style.height = computerSize + 'vh';
+keyboard.style.width = computerSize + 'vh';
 
 const scrollComputerUp = (scrollDistance) => {
     const y = scrollDistance * computerScrollUpSpeed;
@@ -23,11 +30,18 @@ const pauseComputer = (scrollDistance, x) => {
 }
 
 const splitComputer = (scrollDistance) => {
-    const x = (scrollDistance - pauseComputerBreakpoint) * splitComputerSpeed;
+    const scrollDifference = scrollDistance - pauseComputerBreakpoint
+    const x = scrollDifference * splitComputerSpeed;
     const y = scrollDistance - scrollComputerUpBreakpoint * computerScrollUpSpeed;
 
     computer.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0px)';
     keyboard.style.transform = 'translate3d(' + -x + 'px, ' + y + 'px, 0px)';
+
+    const newComputerSize = computerSize + scrollDifference * computerSizeUpSpeed;
+    computer.style.height = newComputerSize + 'vh';
+    computer.style.width = newComputerSize + 'vh';
+    keyboard.style.height = newComputerSize + 'vh';
+    keyboard.style.width = newComputerSize + 'vh';
 }
 
 document.addEventListener("scroll", (evt) => {
