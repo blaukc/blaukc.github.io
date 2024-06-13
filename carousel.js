73f29f87dfed063d42f6f8b19cd453dc1fa0c32e
blaukc <brandonlau1@hotmail.com>
2024-06-13 18:52:12 +0800

    Update carousel css and remove arrows

--- carousel.js
+++ carousel.js
@@ -3,19 +3,14 @@
 const slider = document.querySelector(".carousel-items");
 const slides = document.querySelectorAll(".carousel-item");
 const button = document.querySelectorAll(".carousel-button");
+const activeId = "carousel-active";
+const prevId = "carousel-prev";
+const nextId = "carousel-next"
 
-const numSlides = slider.childElementCount - 1; // minus 1 because we dont count the button container
+const numSlides = slider.childElementCount;
 let current = 0;
 let prev = numSlides - 1;
 let next = 1;
-
-for (let i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
-}
-
-const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
-
-const gotoNext = () => current < numSlides - 1 ? gotoNum(current + 1) : gotoNum(0);
 
 const gotoNum = number => {
     current = number;
@@ -23,9 +18,7 @@ const gotoNum = number => {
     next = current + 1;
 
     for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("carousel-active");
-        slides[i].classList.remove("carousel-prev");
-        slides[i].classList.remove("carousel-next");
+        slides[i].id = ""
     }
 
     if (next == numSlides) {
@@ -36,7 +29,29 @@ const gotoNum = number => {
         prev = numSlides - 1;
     }
 
-    slides[current].classList.add("carousel-active");
-    slides[prev].classList.add("carousel-prev");
-    slides[next].classList.add("carousel-next");
+    slides[current].id = activeId;
+    slides[prev].id = prevId;
+    slides[next].id = nextId;
+}
+
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+const gotoNext = () => current < numSlides - 1 ? gotoNum(current + 1) : gotoNum(0);
+
+const handleCarouselClick = (e) => {
+    let target = e.currenTarget || e.srcElement;
+
+    // Traverse up the DOM tree to find the parent with the ID
+    while (!target.classList.contains("carousel-item")) {
+        target = target.parentElement;
+    }
+
+    if (target.id === prevId) {
+        gotoPrev();
+    } else if (target.id === nextId) {
+        gotoNext();
+    }
+}
+
+for (let i = 0; i < slides.length; i++) {
+    slides[i].addEventListener("click", handleCarouselClick);
 }
